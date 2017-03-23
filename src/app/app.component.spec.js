579e653ad0d4ed0e8797ef2cc7196d4479e9a499
mail2yugi <mail2yugi@gmail.com>
2017-03-24 00:08:27 +0530

    Add files via upload

--- src/app/app.component.spec.js
+++ src/app/app.component.spec.js
@@ -1,21 +1,19 @@
 "use strict";
 var app_component_1 = require('./app.component');
 var testing_1 = require('@angular/core/testing');
-describe('AppComponent', function () {
-    var de;
-    var comp;
-    var fixture;
-    beforeEach(testing_1.async(function () {
-        testing_1.TestBed.configureTestingModule({
-            declarations: [app_component_1.AppComponent]
-        })
-            .compileComponents();
-    }));
-    beforeEach(function () {
-        testing_1.TestBed.compileComponents();
-        fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
-        comp = fixture.componentInstance;
+//describe('AppComponent', function () {
+function main() {
+    describe('AppComponent Test', function () {
+        beforeEach(function () {
+            testing_1.TestBed.configureTestingModule({
+                providers: [
+                    app_component_1.AppComponent
+                ]
+            });
+        });
+        it('Test - AppComponent Should Define.', testing_1.inject([app_component_1.AppComponent], function (cmp) {
+            expect(cmp).toBeDefined();
+        }));
     });
-    testing_1.TestBed.compileComponents();
-    it('should create component', function () { return expect(comp).toBeDefined(); });
-});
+}
+exports.main = main;
